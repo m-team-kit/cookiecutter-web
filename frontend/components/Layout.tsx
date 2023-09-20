8cdf3aedef7f4aa85471e968f541cda6dcab37fd
Christophe <christophe.misc+git@protonmail.ch>
2023-09-20 18:21:11 +0200

    fix(Layout): remove broken leftover Header import

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { FC, PropsWithChildren } from 'react';
-import Header from './Header';
 import clsx from 'clsx';
 
 type LayoutProps = {
